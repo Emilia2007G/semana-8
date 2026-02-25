000514347567a744dd45f2407977f8e782f66954
Your Name <carlaguaman06@gmail.com>
2026-02-25 08:09:53 -0500

    salto de linea

---
+++ salto_linea.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //el universo
+   std::cout << "Hola" << std::endl;    
+   std::cout << "Mundo!\nque\ntal" << std::endl; 
+    
+return 0;
+
+}
